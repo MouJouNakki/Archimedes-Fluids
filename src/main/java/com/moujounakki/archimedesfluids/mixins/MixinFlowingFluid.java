bfe5d87c32e3c0748c4376e2ce564ccac4c13627
MouJouNakki <106318570+MouJouNakki@users.noreply.github.com>
2023-05-29 15:17:35 +0300

    Fix import

--- src/main/java/com/moujounakki/archimedesfluids/mixins/MixinFlowingFluid.java
+++ src/main/java/com/moujounakki/archimedesfluids/mixins/MixinFlowingFluid.java
@@ -1,6 +1,6 @@
 package com.moujounakki.archimedesfluids.mixins;
 
-import com.moujounakki.fluidmotionoverhaul.FluidSpreadType;
+import com.moujounakki.archimedesfluids.FluidSpreadType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
