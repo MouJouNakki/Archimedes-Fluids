4cb78be13fa185062ee41b78f0b401d6fd251a24
MouJouNakki <106318570+MouJouNakki@users.noreply.github.com>
2024-08-06 16:50:20 +0300

    Disable fluidlogging config for now

--- src/main/java/com/moujounakki/archimedesfluids/ArchimedesFluidsCommonConfig.java
+++ src/main/java/com/moujounakki/archimedesfluids/ArchimedesFluidsCommonConfig.java
@@ -14,12 +14,12 @@ public class ArchimedesFluidsCommonConfig {
     private final ConfigValue<Boolean> partialBuckets;
     private final ConfigValue<Integer> flowSpeed;
     private final ConfigValue<Double> puddleSpread;
-    private final ConfigValue<Boolean> fluidlogging;
+    //private final ConfigValue<Boolean> fluidlogging;
     private ArchimedesFluidsCommonConfig(ForgeConfigSpec.Builder builder) {
         partialBuckets = builder.define("partial_buckets", true);
         flowSpeed = builder.define("flow_speed", 1);
         puddleSpread = builder.define("puddle_spread", 0.3);
-        fluidlogging = builder.comment("Can significantly slow down loading, especially when using many mods.", "May not load at all. ").define("fluidlogging", false);
+        //fluidlogging = builder.comment("Can significantly slow down loading, especially when using many mods.", "May not load at all. ").define("fluidlogging", false);
     }
 
     public static boolean getPartialBuckets() {
@@ -32,6 +32,7 @@ public class ArchimedesFluidsCommonConfig {
         return config.getLeft().puddleSpread.get();
     }
     public static boolean getFluidlogging() {
-        return config.getLeft().fluidlogging.get();
+        return false;
+        //return config.getLeft().fluidlogging.get();
     }
 }
